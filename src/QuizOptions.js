ff9ca5f5b07756471993199f9535d3711ead90c5
John <isnocturnal@gmail.com>
2021-11-14 19:28:09 +0000

    refactoring QuizOption to separate component

--- src/QuizOptions.js
+++ src/QuizOptions.js
@@ -1,6 +1,7 @@
 import React, { useState, useRef, useEffect } from 'react';
 import { Redirect } from 'react-router';
 import { county_data } from './data/county_data';
+import './QuizOptions.css';
 
 const BACKGROUND_IMAGE_CORRECT = 'radial-gradient(#7777ff, #777799)'
 const BACKGROUND_IMAGE_WRONG = 'radial-gradient(#ff7777, #997777)'
@@ -8,9 +9,157 @@ const BACKGROUND_IMAGE_UNSELECTED = 'radial-gradient(#dddddd, #999999)';
 const BORDER_COLOR_UNSELECTED = '#999999dd';
 const BORDER_PERSISTENCE_DURATION = 1500;
 const VOLUME_FADE_PERIOD = 1500;
+const APPEARANCE_RESET_TIMEOUT = 3000;
 const CORRECT_SOUND_SOURCE = '/audio/correct_bell.mp3';
 const WRONG_SOUND_SOURCE = '/audio/wrong_buzz.mp3';
 
+
+// Component diplays a grid of county choices, handles clicks on correct
+// and incorrect options and notifies its parent of each correct and incorrect
+// answer. Is also responsible for playing a sound clip for each question, and 
+// an appropriate sound effect to respond to the answer.
+//
+// props :  props.choices  - An array of counties representing the choices
+//          props.correctAnswer - a string; the name of the correct county.
+//          props.notifyParent - callback to inform parent of choice.
+
 function QuizOptions(props) {
-    
+    const audioRef = useRef(null);
+    const soundEffectsRef = useRef(null);
+    let userHasClicked = false;
+    let target = null;
+
+    // Map the possible answers to their images.
+    const imageArray = props.choices.map((county) => {
+        const image_url = '/images/highlighted_counties/' + county + '_highlighted.png';
+        const title = county.charAt(0).toUpperCase() + county.slice(1);
+        let userHasClicked = false;
+        return (
+            <React.Fragment key={county}>
+                <div className='quiz_option_wrapper'>
+                    <div className='question_mask'>
+                    </div>
+                    <div className='quiz_option'
+                        onClick={(event) => { onOptionClick(event, county) }}>
+                        <div className='option_image' style={{
+                            backgroundImage: `url(${image_url})`,
+                            backgroundRepeat: 'no-repeat',
+                            backgroundPosition: 'center',
+                            backgroundSize: 'contain',
+                            padding: '10px',
+                            color: 'black'
+                        }}>
+                        </div>
+                        <div className='option_label'>
+                            {title}
+                        </div>
+                    </div>
+                </div>
+            </React.Fragment>
+        );
+    });
+
+    console.log(imageArray);
+
+    // Load the new audio clip for the correct answer into the audioplayer
+    //
+    //
+    //
+
+    return (
+        <div className='container'>
+            <div className='options_holder'>
+                {imageArray}
+            </div>
+        </div>
+
+    )
+
+    // Handler for click events on the options.
+    const onOptionClick = (event, county) => {
+        if (userHasClicked) {
+            return; // take no action until component is reset
+        } else {
+            userHasClicked = true;
+            let answerIsCorrect = props.correctAnswer == county;
+            console.log('answerIsCorrect : ' + answerIsCorrect + '...' +
+                props.correctAnswer + ' == ' + county);
+            props.informParent(answerIsCorrect);
+
+            // Alter appearance of chosen option to reflect correct/incorrect choice
+            target = event.currentTarget;
+            if (county === props.correctAnswer) {
+                target.style.borderColor = 'blue';
+                target.style.backgroundImage = BACKGROUND_IMAGE_CORRECT;
+            } else {
+                target.style.borderColor = 'red';
+                target.style.backgroundImage = BACKGROUND_IMAGE_WRONG;
+            }
+        }
+    }
+
+    // Hook to manage sound clip fade out after choice is made.
+    useEffect(() => {
+        const volumeFadeInterval = setInterval(() => {
+            var currentVol = audioRef.current.volume;
+            var newVol = (parseFloat(currentVol - 0.1).toFixed(1));
+            if (newVol >= 0) {
+                audioRef.current.volume = newVol;
+            } else {
+                audioRef.current.pause();
+                audioRef.current.currentTime = 0;
+                audioRef.current.volume = 1.0;
+                window.clearInterval(volumeFadeInterval);
+            }
+        }, VOLUME_FADE_PERIOD / 10);
+
+        return () => clearInterval(volumeFadeInterval);
+    }, [userHasClicked]);
+
+    // Hook to manage appearance changes after choice is made.
+    useEffect(() => {
+        let opacityTimeout = setTimeout(() => {
+            setOptionsOpacity(0.0);
+        }, BORDER_PERSISTENCE_DURATION);
+        let appearanceTimeout = setTimeout(() => {
+            //switchBackgroundImage(currentElement, BACKGROUND_IMAGE_UNSELECTED);
+            target.style.borderColor = BORDER_COLOR_UNSELECTED;
+        }, APPEARANCE_RESET_TIMEOUT);
+
+        return () => {
+            clearTimeout(opacityTimeout);
+            clearTimeout(appearanceTimeout);
+        }
+    }, [userHasClicked]);
+
+    const playCurrentClip = () => {
+        audioRef.current.play();
+    }
+
+    const stopCurrentClip = () => {
+        audioRef.current.pause();
+        audioRef.current.currentTime = 0;
+    }
+
+    const playCorrectSound = () => {
+        soundEffectsRef.current.volume = 0.5;
+        soundEffectsRef.current.setAttribute('src', CORRECT_SOUND_SOURCE);
+        soundEffectsRef.current.play();
+    }
+
+    const playWrongSound = () => {
+        soundEffectsRef.current.volume = 0.5;
+        soundEffectsRef.current.setAttribute('src', WRONG_SOUND_SOURCE);
+        soundEffectsRef.current.play();
+    }
+
+    const setOptionsOpacity = (opacity) => {
+        let elements = document.getElementsByClassName('quiz_option');
+        for (const e of elements) {
+            e.style.opacity = opacity;
+        }
+    }
+
 }
+
+export default QuizOptions;
--- src/QuizWidget.js
+++ src/QuizWidget.js
@@ -1,6 +1,7 @@
 import React, { useState, useRef, useEffect } from 'react';
 import { Redirect } from 'react-router';
 import { county_data } from './data/county_data';
+import QuizOptions from './QuizOptions.js';
 import './QuizWidget.css';
 
 const backgroundImageCorrect = 'radial-gradient(#7777ff, #777799)'
@@ -16,7 +17,6 @@ const wrongSoundSource = '/audio/wrong_buzz.mp3';
 let correctChoice = null;
 let currentChoices = [];
 let running = false;
-let isTransition = true;
 
 function QuizWidget(props) {
     const [remainingQs, getRemainingQs] = useState(props.totalNumQs);
@@ -28,51 +28,22 @@ function QuizWidget(props) {
     const [startButtonEnabled, setStartButtonEnabled] = useState(true);
     const [startButtonText, setStartButtonText] = useState('Start Quiz');
     const [quizIsComplete, setQuizIsComplete] = useState(false);
-    const audioRef = useRef(null);
-    const soundEffectsRef = useRef(null);
+    const [currentChoices, setCurrentChoices] = useState([]);
 
-    // function to play audio when a quiz_option is clicked on
-    const onOptionClick = (event, county) => {
-        if (isTransition) {
-            return;
-        }
-        console.log(correctChoice + ' == ' + county);
-        fadeOutCurrentClip();
-        let currentElement = event.currentTarget;
-        setTotalAnswered((totalAnswered) => totalAnswered + 1);
-
-        // Alter appearance of chosen option to reflect correct/incorrect choice
-        if (county === correctChoice) {
-            setUserMessage('Correct!');
-            playCorrectSound();
-            isTransition = true;
-            currentElement.style.borderColor = 'blue';
-            currentElement.style.backgroundImage = backgroundImageCorrect;
-            window.setTimeout(() => {
-                setOptionsOpacity(0.0);
-            }, borderPersistenceDuration);
-            setTotalCorrect((totalCorrect) => totalCorrect + 1);
-        } else {
-            setUserMessage('Wrong!');
-            playWrongSound();
-            isTransition = true;
-            event.currentTarget.style.borderColor = 'red';
-            event.currentTarget.style.backgroundImage = backgroundImageWrong;
-            window.setTimeout(() => {
-                setOptionsOpacity(0.0);
-            }, borderPersistenceDuration);
+    // Function to handle callback from QuizOptions when answer is chosen.
+    const onAnswerChosen = (answerIsCorrect) => {
+        console.log('answerIsCorrect == ' + answerIsCorrect);
+        setTotalAnswered(totalAnswered + 1);
+        if (answerIsCorrect) {
+            setTotalCorrect(totalCorrect + 1);
         }
 
         // If the quiz is still incomplete, after a delay, create and 
         // display the next question.
         window.setTimeout(() => {
             if (running) {
-                switchBackgroundImage(currentElement, backgroundImageUnselected);
-                currentElement.style.borderColor = borderColorUnselected;
                 createNewQuestion();
-                playCurrentClip();
                 setUserMessage('Try this one ...');
-                allowOptionClickAfterDelay();
             }
         }, 3000);
     }
@@ -90,16 +61,9 @@ function QuizWidget(props) {
         }
     }, [totalAnswered]);
 
-    const switchBackgroundImage = (element, img) => {
-        element.style = img;
-    }
-
     const onStartPressed = () => {
         running = true;
         setStartButtonEnabled(false);
-        playCurrentClip();
-        setOptionsOpacity(1.0);
-        allowOptionClickAfterDelay();
     }
 
     // This effect runs once, after the first render, creating a new question.
@@ -117,99 +81,8 @@ function QuizWidget(props) {
         }
 
         // Take the first 4 elements and choose correct answer at random.
-        currentChoices = (countyList.slice(0, 4));
+        setCurrentChoices(countyList.slice(0, 4));
         correctChoice = currentChoices[Math.floor(Math.random() * 4)];
-
-        // Map the possible answers to their images.
-        const imageArray = currentChoices.map((county) => {
-            const image_url = '/images/highlighted_counties/' + county + '_highlighted.png';
-            const title = county.charAt(0).toUpperCase() + county.slice(1);
-            return (
-                <React.Fragment key={county}>
-                    <div className='quiz_option_wrapper'>
-                        <div className='question_mask'>
-
-                        </div>
-                        <div className='quiz_option'
-                            onClick={(event) => { onOptionClick(event, county) }}>
-                            <div className='option_image' style={{
-                                backgroundImage: `url(${image_url})`,
-                                backgroundRepeat: 'no-repeat',
-                                backgroundPosition: 'center',
-                                backgroundSize: 'contain',
-                                padding: '10px',
-                                color: 'black'
-                            }}>
-                            </div>
-                            <div className='option_label'>
-                                {title}
-                            </div>
-                        </div>
-                    </div>
-                </React.Fragment>
-            );
-        });
-
-        // Load the new audio clip for the correct answer into the audioplayer
-        //
-        //
-        //
-
-
-        // Rerender the new set of choice icons and play the current clip
-        setCurrentChoiceIcons(imageArray);
-        if (running) {
-            window.setTimeout(() => { setOptionsOpacity(1.0) }, 500);
-        }
-    }
-
-    const playCurrentClip = () => {
-        audioRef.current.play();
-    }
-
-    const stopCurrentClip = () => {
-        audioRef.current.pause();
-        audioRef.current.currentTime = 0;
-    }
-
-    const fadeOutCurrentClip = () => {
-        const volumeFadeInterval = setInterval(() => {
-            var currentVol = audioRef.current.volume;
-            var newVol = (parseFloat(currentVol - 0.1).toFixed(1));
-            if (newVol >= 0) {
-                audioRef.current.volume = newVol;
-            } else {
-                audioRef.current.pause();
-                audioRef.current.currentTime = 0;
-                audioRef.current.volume = 1.0;
-                window.clearInterval(volumeFadeInterval);
-            }
-        }, volumeFadePeriod / 10);
-    }
-
-    const playCorrectSound = () => {
-        soundEffectsRef.current.volume = 0.5;
-        soundEffectsRef.current.setAttribute('src', correctSoundSource);
-        soundEffectsRef.current.play();
-    }
-
-    const playWrongSound = () => {
-        soundEffectsRef.current.volume = 0.5;
-        soundEffectsRef.current.setAttribute('src', wrongSoundSource);
-        soundEffectsRef.current.play();
-    }
-
-    const setOptionsOpacity = (opacity) => {
-        let elements = document.getElementsByClassName('quiz_option');
-        for (const e of elements) {
-            e.style.opacity = opacity;
-        }
-    }
-
-    const allowOptionClickAfterDelay = () => {
-        window.setTimeout(() => {
-            isTransition = false;
-        }, 1000);
     }
 
     if (quizIsComplete) {
@@ -227,27 +100,17 @@ function QuizWidget(props) {
                         {startButtonText}
                     </button>
                 </div>
-                <div className='options_holder'>
-                    {currentChoiceIcons}
+                <div className='options'>
+                    <QuizOptions choices={currentChoices}
+                        correctAnswer={correctChoice}
+                        notifyParent={onAnswerChosen} />
                 </div>
                 <div className='instructions'>
                     {userMessage}
                 </div>
-                <div>
-                    <audio className='audio_player' ref={audioRef}>
-                        <source src='/audio/file_example_MP3_700KB.mp3' />
-                    </audio>
-                    <audio className='audio_player' ref={soundEffectsRef}>
-                        <source src='/audio/correct_bell.mp3' />
-                    </audio>
-                </div>
             </div>
-
         )
     }
-
-
-
 }
 
 export default QuizWidget;
